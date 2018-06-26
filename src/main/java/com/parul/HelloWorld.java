172ed476759bc202d0c52a7a33b7dc39f817f312
parulsharda <parulsha@thoughtworks.com>
2018-06-26 17:09:03 +0200

    Parul | Hello World

---
+++ src/main/java/com/parul/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.parul;
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        String printMessage = printMessage();
+    }
+
+    public static String printMessage() {
+       return  "Hello World";
+    }
+}
---
+++ src/test/java/com/parul/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.parul;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+
+    @Test
+    public void name() {
+    assertEquals("Hello", HelloWorld.printMessage());
+    }
+}
